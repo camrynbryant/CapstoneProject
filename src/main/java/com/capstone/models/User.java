c09c44e7763d92a21003c2160ac4b20939a49bf3
McBride Kusi <mkusi@charlotte.edu>
2025-04-05 03:03:49 -0400

    Slight change for notification system.

--- src/main/java/com/capstone/models/User.java
+++ src/main/java/com/capstone/models/User.java
@@ -12,21 +12,50 @@ public class User {
     private String email;
     private String password; 
 
+    private boolean notificationsEnabled = true;
+
     public User() {}
 
     public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
-        this.password = password; 
+        this.password = password;
+        this.notificationsEnabled = true; 
     }
 
-    public String getId() { return id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getId() { 
+        return id; 
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getName() { 
+        return name; 
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; } 
+    public void setName(String name) { 
+        this.name = name; 
+    }
+
+    public String getEmail() { 
+        return email; 
+    }
+
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+
+    public String getPassword() { 
+        return password; 
+    }
+
+    public void setPassword(String password) { 
+        this.password = password; 
+    } 
+
+    public boolean isNotificationsEnabled() {
+        return notificationsEnabled;
+    }
+
+    public void setNotificationsEnabled(boolean notificationsEnabled) {
+        this.notificationsEnabled = notificationsEnabled;
+    }
 }
