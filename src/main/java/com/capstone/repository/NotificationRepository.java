ba1a4505e6c9b2210767672f2bd1335ed42fa67a
McBride Kusi <mkusi@charlotte.edu>
2025-04-06 17:33:21 -0400

    Update NotificationRepository.java

--- src/main/java/com/capstone/repository/NotificationRepository.java
+++ src/main/java/com/capstone/repository/NotificationRepository.java
@@ -3,8 +3,15 @@ package com.capstone.repository;
 import com.capstone.models.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationRepository extends MongoRepository<Notification, String> {
     List<Notification> findByUserIdOrderByCreatedAtDesc(String userId);
+
+        long countByUserIdAndReadFalse(String userId);
+
+            List<Notification> findByUserIdAndCreatedAtAfterAndReadFalse(String userId, LocalDateTime time);
+
 }
+
