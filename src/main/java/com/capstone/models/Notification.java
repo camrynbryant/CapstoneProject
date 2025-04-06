64f13b8235eeae1497d4473dd4e29f5aa9a95808
McBride Kusi <mkusi@charlotte.edu>
2025-04-06 17:32:59 -0400

    Update Notification.java for "mark as read" function

--- src/main/java/com/capstone/models/Notification.java
+++ src/main/java/com/capstone/models/Notification.java
@@ -15,12 +15,16 @@ public class Notification {
     private String message;
     private LocalDateTime createdAt;
 
+    private boolean read = false;
+
+
     public Notification() {}
 
     public Notification(String userId, String message) {
         this.userId = userId;
         this.message = message;
         this.createdAt = LocalDateTime.now();
+        this.read = false;
     }
 
     public String getId() {
@@ -54,4 +58,13 @@ public class Notification {
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
     }
