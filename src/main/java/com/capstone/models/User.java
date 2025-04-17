c7fd04485dbbd3f265cc920bbfc7a5c3ecf18f54
McBride Kusi <mkusi@charlotte.edu>
2025-04-17 17:51:13 -0400

    Update User.java

--- src/main/java/com/capstone/models/User.java
+++ src/main/java/com/capstone/models/User.java
@@ -2,17 +2,22 @@ package com.capstone.models;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import java.util.HashSet;
+import java.util.Set;
 
-@Document(collection = "users") 
+@Document(collection = "users")
 public class User {
     @Id
-    private String id; 
-
+    private String id;
     private String name;
     private String email;
-    private String password; 
-
-    private boolean notificationsEnabled = true;
+    private String password;
+    private boolean notificationsEnabled;
+    private String profilePictureUrl;
+    
+    @Field("studyInterests")
+    private Set<String> studyInterests = new HashSet<>();
 
     public User() {}
 
@@ -20,42 +25,64 @@ public class User {
         this.name = name;
         this.email = email;
         this.password = password;
-        this.notificationsEnabled = true; 
+        this.notificationsEnabled = true;
+        this.studyInterests = new HashSet<>();
     }
 
-    public String getId() { 
-        return id; 
+
+    public String getId() {
+        return id;
     }
 
-    public String getName() { 
-        return name; 
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public void setName(String name) { 
-        this.name = name; 
+    public String getName() {
+        return name;
     }
 
-    public String getEmail() { 
-        return email; 
+    public void setName(String name) {
+        this.name = name;
+    }
+  
+    public String getEmail() {
+        return email;
     }
 
-    public void setEmail(String email) { 
-        this.email = email; 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+  
+    public String getPassword() {
+        return password;
     }
 
-    public String getPassword() { 
-        return password; 
+    public void setPassword(String password) {
+        this.password = password;
     }
-
-    public void setPassword(String password) { 
-        this.password = password; 
-    } 
-
+  
     public boolean isNotificationsEnabled() {
         return notificationsEnabled;
     }
 
     public void setNotificationsEnabled(boolean notificationsEnabled) {
         this.notificationsEnabled = notificationsEnabled;
+    }
+  
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
+    
+    public Set<String> getStudyInterests() {
+        return studyInterests;
+    }
+
+    public void setStudyInterests(Set<String> studyInterests) {
+        this.studyInterests = studyInterests;
     }
 }
